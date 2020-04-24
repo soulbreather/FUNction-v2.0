3438331516a336ef7edd47ae7bb2bc361f38f6bd
Samuel Skovbakke <kongsamuel@hotmail.dk>
2020-04-24 10:17:50 +0200

    Co-authored-by: Nikolaj J-K <kagenogmig@gmail.com>

---
+++ projektil.js
@@ -0,0 +1,169 @@
+class Projectile {
+    constructor(radius, projectileType, speed) {
+        // positional variables
+        this.x = -radius;
+        this.y = 0;
+        // this.angle = 0;
+
+        // size and type
+        this.radius = radius;
+        this.projectileType = projectileType;
+        this.speed = speed;
+        this.hasCollided = false;
+        this.col = color(random(0, 255), random(0, 255), random(0, 255));
+
+        // coefficients variables
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+        this.d = 0;
+
+        // it needs to be called once before running and displaying the projectile
+        this.getCoefficients();
+
+    }
+
+    // this function creates and gets random coefficients
+    getCoefficients() {
+        if (this.projectileType == 1) {
+            this.setupLineaer();
+        } else if (this.projectileType == 2) {
+            this.setupParabel();
+        } else if (this.projectileType == 3) {
+            this.setupEksponentiel();
+        } else if (this.projectileType == 4) {
+            this.setupHyperbel();
+        } else if (this.projectileType == 5) {
+            this.setupTrigo();
+        }
+    }
+
+    // this function creates random coefficients for the lineaer function
+    setupLineaer() {
+        let x1 = -this.radius;
+        let y1 = random(0, height);
+        let x2 = width;
+        let y2 = floor(random(0, height));
+        this.a = (y2 - y1) / (x2 - x1);
+        this.b = y1;
+
+    }
+
+    // this function creates random coefficients for the parabola function
+    setupParabel() {
+        let x1 = -this.radius;
+        let y1 = floor(random(0 + 100, height - 100));
+        let x2 = floor(random(width / 2 - 20, width / 2 + 20));
+        let y2 = floor(random(0 - 400, height + 400));
+        let x3 = width;
+        let y3 = floor(random(0, height));
+        this.a = ((x2 - x1) * y3 + (x1 - x3) * y2 + (x3 - x2) * y1) / ((x2 - x1) * x3 * x3 + (x1 * x1 - x2 * x2) * x3 + x1 * x2 * x2 - x1 * x1 * x2);
+        this.b = -(((x2 * x2 - x1 * x1) * y3 + (x1 * x1 - x3 * x3) * y2 + (x3 * x3 - x2 * x2) * y1) / ((x2 - x1) * x3 * x3 + (x1 * x1 - x2 * x2) * x3 + x1 * x2 * x2 - x1 * x1 * x2));
+        this.c = ((x1 * x2 * x2 - x1 * x1 * x2) * y3 + (x1 * x1 * x3 - x1 * x3 * x3) * y2 + (x2 * x3 * x3 - x2 * x2 * x3) * y1) / ((x2 - x1) * x3 * x3 + (x1 * x1 - x2 * x2) * x3 + x1 * x2 * x2 - x1 * x1 * x2);
+    }
+
+    // this function creates random coefficients for the power function
+    setupEksponentiel() {
+        let x1 = 0;
+        let y1 = random(0, height);
+        let x2 = width;
+        let y2 = random(10, height);
+        this.a = pow((y1 / y2), (1 / (x1 - x2)));
+        this.b = (y1 / (pow(this.a, x1)));
+    }
+
+    // this function creates random coefficients for the hyperbolic function
+    setupHyperbel() {
+        let x1 = 50;
+        let y1 = random(0, height);
+        let x2 = width;
+        let y2 = random(0, height);
+        this.a = (x1 * x2 * y1 - x1 * x2 * y2) / (x2 - x1);
+        this.c = (x2 * y2 - x1 * x1) / (x2 - x1);
+    }
+
+    // this function creates random coefficients for the trigonometric function
+    setupTrigo() {
+        this.a = random(80, height / 2);
+        this.b = random(0.001, 0.01);
+        this.c = random(0, TWO_PI);
+        this.d = height / 2 + random(-40, 40);
+    }
+
+    move() {
+        this.x += this.speed;
+        this.y = this.getYValue();
+        // this.angle = 
+    }
+
+    display() {
+        push();
+        fill(this.col);
+        noStroke();
+        circle(this.x, this.y, this.radius * 2);
+        pop();
+    }
+
+    getYValue() {
+        let y = 0;
+        if (this.projectileType == 1) {
+            // lineaer funktion
+            y = this.a * this.x + this.b;
+        } else if (this.projectileType == 2) {
+            // andengrads function
+            y = this.a * pow(this.x, 2) + this.b * this.x + this.c;
+        } else if (this.projectileType == 3) {
+            // eksponentiel function
+            y = this.b * pow(this.a, this.x);
+        } else if (this.projectileType == 4) {
+            // hyperbole function
+            y = this.a * pow(this.x, -1) + this.c;
+        } else if (this.projectileType == 5) {
+            // trigo function
+            y = this.a * sin(this.b * this.x + this.c) + this.d;
+        }
+        return y;
+    }
+}
+
+class Player {
+    constructor(x, radius, speed) {
+        this.x = x;
+        this.y = height / 2;
+        this.radius = radius;
+        this.speed = speed;
+        this.dir = 0;
+    }
+
+    move() {
+        this.y += this.dir * this.speed;
+        // limit inside screen
+        if (this.y <= this.radius) {
+            this.y = this.radius;
+        } else if (this.y >= height - this.radius) {
+            this.y = height - this.radius;
+        }
+    }
+
+    display() {
+        push();
+        fill(color(255, 0, 0));
+        stroke(180);
+        circle(this.x, this.y, this.radius * 2);
+        pop();
+    }
+}
+
+class Bullet {
+    constructor(x, y, radius, speed) {
+
+    }
+
+    move() {
+
+    }
+
+    display() {
+
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,52 @@
+let tempAmount = 10;
+let player;
+let projectiles = [];
+
 function setup() {
-  createCanvas(1280, 720);
+  // Creates the canvas in a div with the id of 'canvas-holder'
+  windowWidth = windowWidth * 0.723;
+  canvas = createCanvas(windowWidth, windowHeight * 0.9);
+  canvas.parent('canvas-holder');
+  for (let i = 0; i < tempAmount; i++) {
+    projectiles[i] = new Projectile(20, floor(random(1, 5.9)), random(3, 6));
+  }
+  player = new Player(width - 100, 20, 6);
 }
 
 function draw() {
   background(220);
+  for (let i = 0; i < tempAmount; i++) {
+    projectiles[i].move();
+    projectiles[i].display();
+    // respawn if out of screen
+    if (projectiles[i].x > width + projectiles[i].radius) {
+      projectiles[i].x = -projectiles[i].radius;
+      projectiles[i].getCoefficients();
+    }
+  }
+  player.move();
+  player.display();
+}
+
+function keyPressed() {
+  if (keyCode == UP_ARROW || key == "w") {
+    player.dir -= 1;
+  }
+  if (keyCode == DOWN_ARROW || key == "s") {
+    player.dir += 1;
+  }
+}
+
+function keyReleased() {
+  if (keyCode == UP_ARROW || key == "w") {
+    player.dir += 1;
+  }
+  if (keyCode == DOWN_ARROW || key == "s") {
+    player.dir -= 1;
+  }
+}
+
+// Makes canvas responsive aka. always the full span of the browser
+function windowResized() {
+  resizeCanvas(windowWidth * 0.723, windowHeight * 0.9);
 }
